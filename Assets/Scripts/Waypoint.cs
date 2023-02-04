2b6d31c9f57d434e8f6602266466f04726625ba1
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-02-04 13:35:35 +0800

    Create bool is placeble for world tiles

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private bool isPlaceable;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        if (isPlaceable) Debug.Log(transform.name);
     }
 }
