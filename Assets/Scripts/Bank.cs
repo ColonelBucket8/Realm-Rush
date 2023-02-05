2e187537e71c48d8ca3bdb483d9c325e78cd8b32
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-02-05 19:54:53 +0800

    Add gold balance text at the bottom left

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,31 +1,41 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour
 {
     [SerializeField] private int startingBalance = 150;
+    [SerializeField] private TextMeshProUGUI goldText;
 
     public int CurrentBalance { get; private set; }
 
     private void Awake()
     {
         CurrentBalance = startingBalance;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        goldText.text = "Gold: " + CurrentBalance;
     }
 
     public void Deposit(int amount)
     {
         CurrentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         CurrentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
         if (CurrentBalance < 0)
             // lose the game
             ReloadScene();
     }
 
-    private void ReloadScene()
+    private static void ReloadScene()
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
